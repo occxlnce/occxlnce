76d8327d1b2d05a12e48624f1a5b80d02e5a434b
Occxlnce office <110081105+El-Sombra-lab@users.noreply.github.com>
2024-01-19 09:29:02 +0200

    Update oversight.js

--- oversight.js
+++ oversight.js
@@ -3,9 +3,9 @@ const text = document.querySelector('.typing-text');
 
 // make a words array
 const words = [
-  "We are a creative studio that highlights a collection of diverse projects and creative endeavors.",
-  "These projects encompass a wide range of fields, from web development and brand identity to design work",
-  "Our website offers insights into the creative process and the outcomes of each project."
+  "We are a creative studio that highlights a collection of diverse projects & creative endeavors.",
+  "These projects encompass a wide range of fields, from development & brand identity to design work",
+  "Our website offers insights into the creative process & the outcomes of each project."
 ];
 
 
